f78a8f20e86fe9799f6d62001a5bff6784204b41
DaliaEltohami <87623083+DaliaEltohami@users.noreply.github.com>
2021-08-09 23:21:43 +0200

    Update app.js
    In this last step I define function to update the UI elements with returned data from the app endpoint, chain promise to fetch data from app endpoint after finish posting, then call the updateUI function using the returned data

--- My Project/app.js
+++ My Project/app.js
@@ -9,11 +9,15 @@ const personalAPIKey = "&appid=e54731bc5eadb2080db2679daaabc6bf";
 const zipTextBox = document.getElementById('zip');
 // Define variable to get the button element to create event listener on it 
 const button = document.getElementById('generate');
+// Define three variables to get the html elements that will hold the UI updated data
+const date = document.getElementById('date');
+const temp = document.getElementById('temp');
+const content = document.getElementById('content');
 
 const userResponseTextBox = document.getElementById('feelings');
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Add event listener for the button element when clicked to perform some actions
 button.addEventListener('click',performActions);
@@ -40,6 +44,15 @@ function performActions(e){
         // console.log(data.main.temp)
         postEndPointData('/postData',{temperature : data.main.temp , date : newDate , userResponse : userResponse})
     })
+    // Chain another promise to get the data from app endpoint
+    .then(()=>{
+        getEndPointData('/all')
+        // Chain another promise to update the UI after finishing the get request to the app endpoint using
+        // the data returned from the previous promise
+        .then(data=>{
+            updateUI(data);
+        })
+    })
 }
 
 // Define the asyn function getAPIData
@@ -63,15 +76,15 @@ const getAPIData = async (url)=>{
 const postEndPointData = async (url= '', data = {})=>{
     // Make the post request fetch call using the url to the app endpoint and object data that will be 
     // stored in the app endpoint object which id projectData 
-    const response = await fetch(url, {
-        method : 'POST',
-        credentials : 'same-origin',
-        headers :{
-            'Content-Type': 'Application/json'
-        },
-        body : JSON.stringify(data)
-    })
     try{
+        const response = await fetch(url, {
+            method : 'POST',
+            credentials : 'same-origin',
+            headers :{
+                'Content-Type': 'Application/json'
+            },
+            body : JSON.stringify(data)
+        })
         // confirm the posting of the data 
         console.log('Data Posted');
     }
@@ -84,9 +97,20 @@ const postEndPointData = async (url= '', data = {})=>{
 const getEndPointData = async (url = '')=>{
     const response = await fetch (url);
     try{
-        const EndPointData = await response.json();
+        const endPointData = await response.json();
+        console.log(endPointData);
+        return endPointData;
     }
     catch(error){
         console.log('error',error);
     }
+}
+
+// Define function that will update the UI data using the data returned from the app endpoint
+function updateUI(data){
+    
+    // Set the innerHTML property for the divs in entry holder to the values of data returned from the app endpoint
+    date.innerHTML = `<p>Date : ${data.date}</p>`;
+    temp.innerHTML = `<p>Temperature : ${data.temperature} C</p>`;
+    content.innerHTML = `<p>User Response : ${data.userResponse}</p>`;
 }
