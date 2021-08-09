0f14a3c9b4583ad99d4dd5d8c23cb6100bb52249
DaliaEltohami <87623083+DaliaEltohami@users.noreply.github.com>
2021-08-09 02:42:07 +0200

    Update app.js
    Make another async function to post the data to the app endpoint, chain promises so that we will use the received data from the previous get request in the next post request

--- My Project/app.js
+++ My Project/app.js
@@ -9,6 +9,8 @@ const personalAPIKey = "&appid=e54731bc5eadb2080db2679daaabc6bf";
 const zipTextBox = document.getElementById('zip');
 // Define variable to get the button element to create event listener on it 
 const button = document.getElementById('generate');
+
+const userResponseTextBox = document.getElementById('feelings');
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
@@ -24,8 +26,20 @@ function performActions(e){
     // Make the full URL for the API using three variables
     APIFullUrl = baseURL+zipCode+personalAPIKey+'&units=metric';
     // console.log(APIFullUrl);
+
+    // Get the user response value from the html element and if no value set a default value
+    let userResponse = "No User Response";
+    if(userResponseTextBox.value!==undefined){
+        userResponse = userResponseTextBox.value
+    }
+
     // Call the asyn getAPIData function to make get fetch call to the api url 
-    getAPIData(APIFullUrl);
+    getAPIData(APIFullUrl)
+    // chain another promise that will call the function to make post request to the app endpoint 
+    .then(data=>{
+        // console.log(data.main.temp)
+        postEndPointData('/postData',{temperature : data.main.temp , date : newDate , userResponse : userResponse})
+    })
 }
 
 // Define the asyn function getAPIData
@@ -35,7 +49,7 @@ const getAPIData = async (url)=>{
     try{
         // Convert the received data from the response to json format
         const returnedData = await response.json();
-        // console.log(returnedData);
+        console.log(returnedData);
         // Return the data 
         return returnedData;
     }
@@ -45,3 +59,34 @@ const getAPIData = async (url)=>{
     }
 }
 
+// Define async funcion that will use a fetch call to make post request with url and object data 
+const postEndPointData = async (url= '', data = {})=>{
+    // Make the post request fetch call using the url to the app endpoint and object data that will be 
+    // stored in the app endpoint object which id projectData 
+    const response = await fetch(url, {
+        method : 'POST',
+        credentials : 'same-origin',
+        headers :{
+            'Content-Type': 'Application/json'
+        },
+        body : JSON.stringify(data)
+    })
+    try{
+        // confirm the posting of the data 
+        console.log('Data Posted');
+    }
+    catch(error){
+        console.log('error',error);
+    }
+}
+
+// Define async function to fetch the data from the app endpoint
+const getEndPointData = async (url = '')=>{
+    const response = await fetch (url);
+    try{
+        const EndPointData = await response.json();
+    }
+    catch(error){
+        console.log('error',error);
+    }
+}
